816a490a00b944812d03cfc6f780a559a6fbb197
prateek0128 <rajput.prateek28@gmail.com>
2025-10-16 12:36:01 +0530

    API Utility Updated

--- src/api.ts
+++ src/api.ts
@@ -1,9 +1,11 @@
 import axios from "axios";
 
+//const baseURL = "http://127.0.0.1:8080";
+const baseURL = "https://marketbriefs.co.in";
+const apiUrl = `${baseURL}/api/v1`;
 export const API = axios.create({
-  baseURL: "http://127.0.0.1:8080/api/v1",
+  baseURL: apiUrl,
 });
-
 
 API.interceptors.request.use((config) => {
   const token = localStorage.getItem("access_token");
